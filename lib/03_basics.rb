42b9a6f907b5bb7cef4fe8c36ca1138efc8bfdff
amacarle <59627436+amacarle@users.noreply.github.com>
2020-01-21 11:24:56 +0100

    Add files via upload

---
+++ lib/03_basics.rb
@@ -0,0 +1,19 @@
+def who_is_bigger(a,b,c)
+	if a==nil || b==nil || c==nil
+          return 'nil detected'
+        elseif a>b && a>c
+          return "a is bigger"
+        elseif b>a && b>c
+          return "b is bigger"
+        elseif c>a && c>b
+          return "c is bigger"
+        end
+end
+ 
+
+def array_42(i)
+
+  array.include? 42
+end
+
+
---
+++ lib/04_simon_says.rb
@@ -0,0 +1,19 @@
+def echo(string)
+	if a==nil || b==nil || c==nil
+          return "nil detected"
+        elseif a>b && a>c
+          return "a is bigger"
+        elseif b>a && b>c
+          return "b is bigger"
+        else
+          return "c is bigger"
+        end
+end
+ 
+
+def array_42
+
+  array.include? 42
+end
+
+
